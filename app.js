aa10ed8cbb5c0d96f34ec6b1c0e34515febebc07
jidkd <jidk.daniels@gmail.com>
2022-03-06 19:44:43 +0200

    lauren

--- app.js
+++ app.js
@@ -1,11 +1,38 @@
+// Server
 const app = require("express")();
 
 const PORT = process.env.PORT || 3000;
 
 app.get("", (req, res) => {
-    res.send("Hello World");
+    res.send("Hello Lauren");
 })
 
-app.listen(PORT, () => {
+ app.listen(PORT, async () =>  {
     console.log(`App up at port ${PORT}`);
 })
+
+// Search API
+const SerpApi = require('google-search-results-nodejs');
+const search = new SerpApi.GoogleSearch("3cb2c54d231f24b4fa958a59bcee6f15a038aad59937532b5957d8515747d458");
+
+const params = {
+    q: "Apple",
+    tbm: "isch",
+    ijn: "0"
+  };
+
+search.json(params, callback);
+
+const callback = async function(data) {
+    x = data['images_results'][0]
+    console.log(x);
+    return x;
+};
+
+
+
+
+
+
+console.log("----------------------------------------------")
+// console.log(x)
