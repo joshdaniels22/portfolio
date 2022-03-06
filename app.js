a66ab730e7f244e855f9a92bb42da19ba5c201c4
jidkd <jidk.daniels@gmail.com>
2022-03-06 21:03:30 +0200

    feature: displays html

--- app.js
+++ app.js
@@ -1,40 +1,40 @@
 // Server
 const app = require("express")();
-
 const PORT = process.env.PORT || 3000;
-
-app.get("", (req, res) => {
-    res.send("Hello Lauren");
-})
-
- app.listen(PORT, async () =>  {
-    console.log(`App up at port ${PORT}`);
-})
-
-// Search API
 const SerpApi = require('google-search-results-nodejs');
 const search = new SerpApi.GoogleSearch("3cb2c54d231f24b4fa958a59bcee6f15a038aad59937532b5957d8515747d458");
-
 const params = {
-    q: "Apple",
+    q: "cats",
     tbm: "isch",
     ijn: "0"
   };
 
-  const callback = async function(data) {
-    x = data['images_results'][0]
-    console.log(x);
-    return x;
+app.get("", (req, res) => {
+    //search.json(params, callback);
+    //console.log(path.join(__dirname, '/index.html'))
+    //console.log(__dirname+ '/index.html')
+    res.sendFile( __dirname+ '/index.html');
+    // res.send("Hello Lauren");
+})
+
+ app.listen(PORT, async () =>  {
+    //console.log("------------",__dirname, '\index.html')
+    console.log(`App up at port ${PORT}`);
+})
+
+// Search API
+
+
+
+const callback = function(data) {
+    displayPic(data['images_results'][4]);
+    // x = 
+    // console.log(x);
+    // return x;
 };
 
-search.json(params, callback);
 
 
-
-
-
-
-
-
-console.log("----------------------------------------------")
-// console.log(x)
+function displayPic(entry){
+    console.log(entry['original']);
+}
