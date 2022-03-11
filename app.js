1b02dd4f5bb7fc6a392c8ec9a160ba679e37d761
jidkd <jidk.daniels@gmail.com>
2022-03-11 22:25:12 +0200

    test: dapi off

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const params = {
   };
 
 app.get("", (req, res) => {
-    search.json(params, callback);
+    // search.json(params, callback);
     //console.log(path.join(__dirname, '/index.html'))
     //console.log(__dirname+ '/index.html')
     res.send(__dirname+ '/index.html')
