3617906f20c8f34d497b7afd3cd3152507aa6398
jidkd <jidk.daniels@gmail.com>
2022-03-12 22:54:00 +0200

    implementing api

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 // Server
 const app = require("express")();
 const PORT = process.env.PORT || 3000;
-// const SerpApi = require('google-search-results-nodejs');
-// const search = new SerpApi.GoogleSearch("3cb2c54d231f24b4fa958a59bcee6f15a038aad59937532b5957d8515747d458");
-// const params = {
-//     q: "cats",
-//     tbm: "isch",
-//     ijn: "0"
-//   };
+const SerpApi = require('google-search-results-nodejs');
+const search = new SerpApi.GoogleSearch("3cb2c54d231f24b4fa958a59bcee6f15a038aad59937532b5957d8515747d458");
+const params = {
+    q: "cats",
+    tbm: "isch",
+    ijn: "0"
+  };
 
 app.get("", (req, res) => {
     // search.json(params, callback);
